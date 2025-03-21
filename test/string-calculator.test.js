9a66e9f714dec4158538c4b835c21f965d69fb42
kg05 <kavishgupta86@gmail.com>
2025-03-22 03:47:10 +0530

    added failing test case to handle the newline bwtween number

--- test/string-calculator.test.js
+++ test/string-calculator.test.js
@@ -15,3 +15,7 @@ test("should return sum of two comma-separated numbers", () => {
 test("should return sum of multiple numbers", () => {
   expect(add("1,2,3,4")).toBe(10);
 });
+
+test("should handles new lines between number", () => {
+  expect(add("1\n2,3")).toBe(6);
+});
