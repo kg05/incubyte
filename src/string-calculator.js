9e611b2e27f6ba39bcd2baf7d3790482b98acfa5
kg05 <kavishgupta86@gmail.com>
2025-03-22 03:40:56 +0530

    right way to handle when 2 comma separated number is given

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,7 +1,8 @@
 function add(numStr) {
   if (numStr == "") return 0;
   const numbers = numStr.split(",").map(Number);
-  return numbers.reduce((sum, num) => sum + num, 0);
+  if (numbers.length == 1) return numbers[0];
+  return numbers[0] + numbers[1];
 }
 
 module.exports = add;
