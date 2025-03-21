1e7bc228014d68b626fec5c637d2db03666f9315
kg05 <kavishgupta86@gmail.com>
2025-03-22 03:29:03 +0530

    added the failing test case for single number

--- test/string-calculator.test.js
+++ test/string-calculator.test.js
@@ -1,5 +1,9 @@
 const add = require("../src/string-calculator");
 
-test("returns 0 for an empty string", () => {
+test("should return 0 when an empty string is passed", () => {
   expect(add("")).toBe(0);
+});
+
+test("should return the number itself when a single number is passed", () => {
+  expect(add("1")).toBe(1);
 });
